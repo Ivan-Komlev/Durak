8f26e41880939b122105413150be51e3920562b3
JoomlaBoat <ivankomlev@gmail.com>
2020-11-12 04:41:37 -0500

    Card drag and drop added.

---
+++ Card.py
@@ -0,0 +1,37 @@
+
+import arcade
+
+# Face down image
+FACE_DOWN_IMAGE = "src/red_back.png"
+
+class Card(arcade.Sprite):
+    """ Card sprite """
+
+    def __init__(self, suit, value, scale=1):
+        """ Card constructor """
+
+        # Attributes for suit and value
+        self.suit = suit
+        self.value = value
+
+        # Image to use for the sprite when face up
+        self.image_file_name = f"src/{self.value}{self.suit}.png"
+        
+        self.is_face_up = False
+        super().__init__(FACE_DOWN_IMAGE, scale, False)
+
+    def face_down(self):
+        """ Turn card face-down """
+        self.texture = arcade.load_texture(FACE_DOWN_IMAGE)
+        self.is_face_up = False
+
+    def face_up(self):
+        """ Turn card face-up """
+        self.texture = arcade.load_texture(self.image_file_name)
+        
+        self.is_face_up = True
+
+    @property
+    def is_face_down(self):
+        """ Is this card face down? """
+        return not self.is_face_up
--- Durak.py
+++ Durak.py
@@ -7,39 +7,31 @@
  * @copyright Copyright (C) 2020. All Rights Reserved
  * @license GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
  
+ 
+ * This is the Project #3: Pick a Card Game! of Pirple.com - Python is Easy
+ *  - Everyone has their favorite card game. What's yours? For this assignment, choose a card game (other than Blackjack), and turn it into a Python program.
+ *  - It doesn't matter if it's a 1-player game, or a 2 player game, or more! That's totally up to you. A few requirements:
+ *
+ *  1. It's got to be a card game (no board games, etc)
+ *  2. When the game starts up, you should ask for the players' names. And after they enter their names, your game should refer to them by name only. 
+ *  ("It's John's turn" instead of "It's player 1's turn). 
+ *  3. At any point during the game, someone should be able to type "--help" to be taken to a screen where they can read the rules of the game and instructions for how to play.
+ *  After they're done reading, they should be able to type "--resume" to go back to the game and pick up where they left off.
+ *
+ * Card moving tutorial: https://arcade.academy/tutorials/card_game/index.html
+ * GUI Elements: https://arcade.academy/examples/gui_elements.html
 """
 
-import random, pygame
-from pygame.locals import *
+import arcade
 
-FPS = 60
-WINDOWWIDTH = 800
-WINDOWHEIGHT = 600
+import arcade.gui
+from arcade.gui import UIManager
 
-BGCOLOR = ( 0, 55, 0)
-
-def main():
-    global FPSCLOCK, DISPLAYSURF, BASICFONT
-
-    pygame.init()
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
-    pygame.display.set_caption('Durak')
-
-    while True:
-        runGame()
-        #showGameOverScreen()
-
-def runGame():
-    DISPLAYSURF.fill(BGCOLOR)
-
-    carImg = pygame.image.load('src/2C.png')
-    DISPLAYSURF.blit(carImg, (0,0))
-
-    pygame.display.update()
-    FPSCLOCK.tick(FPS)
-    
+from Table import Table
 
 if __name__ == '__main__':
-    main()
+    theTable = Table()
+    theTable.setup()
+
+    arcade.run()
+
